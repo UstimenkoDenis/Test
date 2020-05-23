b89ebc3ff833ce60689140ac5028fba077ea6d43
Ustimenko Denis <ustimenkodenis@mail.ru>
2020-05-23 19:07:14 +0300

    Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AuthLayoutComponent} from './shared/layouts/auth-layout/auth-layout.component';
+import {SiteLayoutComponent} from './shared/layouts/site-layout/site-layout.component';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {RegisterPageComponent} from './register-page/register-page.component';
 
+const routes: Routes = [
+  {
+    path: '', component: AuthLayoutComponent, children: [
+      { path: '', redirectTo: '/login', pathMatch: 'full'},
+      { path: 'login', component: LoginPageComponent },
+      { path: 'register', component: RegisterPageComponent }
 
-const routes: Routes = [];
+    ]
+  },
+  {
+    path: '', component: SiteLayoutComponent, children: [
+      
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
